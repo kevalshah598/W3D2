04d0f69682c1b2a871aa9d8132a4d70d2335932e
kevalshah598 <kevalshah598@gmail.com>
2023-01-24 17:02:53 -0500

    adding pairs as objects into the class

--- memory puzzle/card.rb
+++ memory puzzle/card.rb
@@ -1,24 +1,51 @@
 class Card
+    def shuffled_pairs(n)
+        n.times do
+            random_array = ("a".."z").to_a.shuffle
+            value1 = random_array[0]
+            value2 = value1
+            pair = [value1, value2]
+            pair.each do |card|
+                self.new(card)
+            end
+        end
+    end
 
-    def initialize(card)
-        @card = card
+    def initialize(value, revealed = false)
+      @value = value
+      @revealed = revealed
+    end
+
+    def value
+        @value
     end
 
     def hide
-        
+      @revealed = false
+    end
+    
+    def reveal 
+        @revealed = true
     end
 
-    def show
-        @face_up = true
-    end
-
-    def reveal
-        if self.show
-
-        end
-    end
 end
 
-card = Card.new
-p card.hide
-@letters = rand("a".."z")
+    random_array = ("a".."z").to_a.shuffle
+    value = random_array[0]
+    value2 = value 
+    array = [value, value2]
+    p array
+
+    def shuffled_pairs(n)
+        n.times do
+            random_array = ("a".."z").to_a.shuffle
+            value1 = random_array[0]
+            value2 = value1
+            pair = [value1, value2]
+            pair.each do |card|
+                p card
+            end
+        end
+    end
+
+    shuffled_pairs(5)
